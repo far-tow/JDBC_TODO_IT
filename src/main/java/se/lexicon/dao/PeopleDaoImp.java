7500919a3206dd7725753e9f566e4e7548a0739a
Farhad Towfighian <fartow@gmail.com>
2022-12-22 20:42:09 +0100

    People-part is done!

--- src/main/java/se/lexicon/dao/PeopleDaoImp.java
+++ src/main/java/se/lexicon/dao/PeopleDaoImp.java
@@ -169,3 +169,4 @@ public class PeopleDaoImp implements PeopleDao {
         return del;
     }
 }
+
--- src/main/java/se/lexicon/model/Person.java
+++ src/main/java/se/lexicon/model/Person.java
@@ -55,4 +55,5 @@ public class Person {
                 ", LastName = '" + lastName + '\'' +
                 '}';
     }
+
 }
