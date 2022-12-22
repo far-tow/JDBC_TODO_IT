52e1654ee85657500a70baf16c21903d7c044fa0
Farhad Towfighian <fartow@gmail.com>
2022-12-22 22:37:58 +0100

    Starting TodoItemsDaoImp.

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -2,8 +2,12 @@ package se.lexicon;
 
 import se.lexicon.dao.PeopleDao;
 import se.lexicon.dao.PeopleDaoImp;
+import se.lexicon.dao.TodoItemsDao;
+import se.lexicon.dao.TodoItemsDaoImp;
 import se.lexicon.model.Person;
+import se.lexicon.model.Todo;
 
+import java.time.LocalDate;
 import java.util.Collection;
 
 public class App {
@@ -12,15 +16,16 @@ public class App {
     public static void main(String[] args) {
 
         PeopleDao pd = new PeopleDaoImp();
-
+        TodoItemsDao to_do = new TodoItemsDaoImp();
+// _________________ PERSON ______________//
         // CREATE
         /*Person person = new Person (6,"Mehrdad", "Javan");
         pd.create(person);
         System.out.println("person = " + person);*/
 
         // findAll
-        Collection<Person> all = pd.findAll();
-        all.forEach(System.out::println);
+        /*Collection<Person> all = pd.findAll();
+        all.forEach(System.out::println);*/
 
         //findById
        /* Person  person = pd.findById(5);
@@ -31,13 +36,20 @@ public class App {
         names.forEach(System.out::println);*/
 
         //UPDATE
-        Person person = pd.findById(6);
+       /* Person person = pd.findById(6);
         person.setFirstName("TestUpdate");
         person.setLastName("Updatesson");
-        pd.update(person);
+        pd.update(person);*/
 
         //DELETE
         //boolean del = pd.deleteById(3);
+
+        // _________________ TO DO ______________//
+
+        //CREATE
+        Todo todo = new Todo(1, "Write Code", "Write some Java code", LocalDate.of(2022,12,22), false,1);
+        todo = to_do.create(todo);
+        System.out.println("todo = " + todo);
 
 
     }
--- src/main/java/se/lexicon/dao/TodoItemsDaoImp.java
+++ src/main/java/se/lexicon/dao/TodoItemsDaoImp.java
@@ -1,15 +1,46 @@
 package se.lexicon.dao;
 
+import se.lexicon.dao.dataBase.DbConnection;
+import se.lexicon.exeptions.DBConnectionException;
 import se.lexicon.model.Person;
 import se.lexicon.model.Todo;
 
+import java.sql.*;
 import java.util.Collection;
 
-public class TodoItemsDaoImp implements TodoItemsDao{
+public class TodoItemsDaoImp implements TodoItemsDao {
     public Todo create(Todo todo) {
+        String query = "INSERT INTO todo_item (todo_id, title, description, deadline, done, assignee_id) VALUES (?,?,?,?,?,?)";
+        Todo addedItem = null;
 
-        return null;
-    }
+        try (
+                Connection connection = DbConnection.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        ) {
+            preparedStatement.setInt(1, todo.getTodoId());
+            preparedStatement.setString(2, todo.getTitle());
+            preparedStatement.setString(3, todo.getDescription());
+            preparedStatement.setString(4, String.valueOf(todo.getDeadLine()));
+            preparedStatement.setBoolean(5, todo.isDone());
+            preparedStatement.setInt(6, todo.getAssigneeId());
+
+            int result = preparedStatement.executeUpdate();
+            System.out.println((result == 1) ? "Added successfully" : "Not added!");
+            try (ResultSet resultSet = preparedStatement.getGeneratedKeys();) {
+
+                if (resultSet.next()) {
+                    addedItem = new Todo(resultSet.getInt(1),
+                            todo.getTitle(), todo.getDescription(), todo.getDeadLine(), todo.isDone(), todo.getAssigneeId());
+                }
+                todo = addedItem;
+            }
+
+        } catch (DBConnectionException | SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return todo;
+}
 
     public Collection<Todo> findAll() {
         return null;
